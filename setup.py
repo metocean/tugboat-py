5b46d6743b50489d7da843fe80888976c2f760ca
Thomas Coats <github@voodoolabs.net>
2016-03-08 22:30:51 +1300

    0.5.1 Docker Compose 1.6.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")
 
 install_requires = [
-    'docker-compose >= 1.4.0, < 1.5'
+    'docker-compose >= 1.6.0, < 1.7'
 ]
 
 setup(
